053981dced3f85aadf678b7ab8b377e634c213cf
Adam Zverbik <zverbik.a@gmail.com>
2022-02-20 11:04:05 +0100

    Configure i2s bus

---
+++ sound_recognition/main.h
@@ -0,0 +1,23 @@
+#ifndef main_h
+#define main_h
+
+// Set pins
+#define PIN_I2S_WS GPIO_NUM_22  // Word Select
+#define PIN_I2S_SD GPIO_NUM_21  // Serial Data
+#define PIN_I2S_SCK GPIO_NUM_26 // Serial Clock
+
+// Config I2S
+#define I2S_PORT I2S_NUM_0
+#define I2S_READ_LEN (16 * 1024)
+
+// Config DMA
+#define DMA_BUF_COUNT 16 
+#define DMA_BUF_LEN 64
+
+// Config audio
+#define BITS_PER_SAMPLE 16
+#define SAMPLE_RATE 16000
+#define NUM_CHANNELS 1
+
+
+#endif
--- sound_recognition/sound_analysis.h
+++ sound_recognition/sound_analysis.h
@@ -1,71 +1,69 @@
-#include "esphome.h"
+#include "main.h"
+#include <esphome.h>
 #include <driver/i2s.h>
 
-#define SAMPLE_BUFFER_SIZE 512
-#define SAMPLE_RATE 8000
-// most microphones will probably default to left channel but you may need to tie the L/R pin low
-#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
-// either wire your microphone to the same pins or change these to match your wiring
-#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_26      // 2
-#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_22  // 15
-#define I2S_MIC_SERIAL_DATA GPIO_NUM_21       // 13
+uint8_t buffer32[4096];
 
-// don't mess around with this
-i2s_config_t i2s_config = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
-    .sample_rate = SAMPLE_RATE,
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-    .communication_format = I2S_COMM_FORMAT_I2S,
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = 4,
-    .dma_buf_len = 1024,
-    .use_apll = false,
-    .tx_desc_auto_clear = false,
-    .fixed_mclk = 0};
+void init_i2s()
+{
+  // Config struct
+  i2s_config_t i2s_config = {
+      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+      .sample_rate = SAMPLE_RATE,
+      .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+      .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+      .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+      .intr_alloc_flags = 0,
+      .dma_buf_count = DMA_BUF_COUNT,
+      .dma_buf_len = DMA_BUF_LEN,
+      .use_apll = false,
+  };
 
-// and don't mess around with this
-i2s_pin_config_t i2s_mic_pins = {
-    .bck_io_num = I2S_MIC_SERIAL_CLOCK,
-    .ws_io_num = I2S_MIC_LEFT_RIGHT_CLOCK,
-    .data_out_num = I2S_PIN_NO_CHANGE,
-    .data_in_num = I2S_MIC_SERIAL_DATA};
+  // Config GPIO
+  const i2s_pin_config_t pin_config = {
+      .bck_io_num = PIN_I2S_SCK,
+      .ws_io_num = PIN_I2S_WS,
+      .data_out_num = I2S_PIN_NO_CHANGE,
+      .data_in_num = PIN_I2S_SD};
+
+  // Init driver
+  i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+
+  // Init GPIO
+  i2s_set_pin(I2S_PORT, &pin_config);
+}
+
+void read_data()
+{
+  uint32_t bytes_read = 0;
+  uint16_t samples_read;
+  Serial.println("Recording started!");
+  i2s_read(I2S_PORT, buffer32, sizeof(buffer32), &bytes_read, portMAX_DELAY);
+
+  samples_read = bytes_read >> 2;
+
+  for (uint32_t i = 0; i < samples_read; i++)
+  {
+    uint8_t lsb = buffer32[i * 4 + 2];
+    uint8_t msb = buffer32[i * 4 + 3];
+    uint16_t sample = ((((uint16_t)msb) << 8) | ((uint16_t)lsb)) << 4;
+
+      //Serial.printf("%ld\n", sample);
+  }
+}
+
 
 
 class SoundAnalysis : public Component, public CustomMQTTDevice {
  public:
   void setup() override {
     // start up the I2S peripheral
-    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-    i2s_set_pin(I2S_NUM_0, &i2s_mic_pins);
+    init_i2s();
 
-    // subscribe("the/topic", &SoundAnalysis::on_message);
+    delay(500);
   }
-
-int32_t raw_samples[SAMPLE_BUFFER_SIZE];
 
   void loop() override {
-    // read from the I2S device
-    size_t bytes_read = 0;
-    i2s_read(I2S_NUM_0, raw_samples, sizeof(int32_t) * SAMPLE_BUFFER_SIZE, &bytes_read, portMAX_DELAY);
-    int samples_read = bytes_read / sizeof(int32_t);
-    // dump the samples out to the serial channel.
-    for (int i = 0; i < samples_read; i++)
-    {
-      // logging value into ESPHome logger
-      // commented, because HA doesn't like getting so many values in so short time
-      // ESP_LOGD("custom", "%ld\n", raw_samples[i]);
-    }
+    read_data();
   }
-
-
-  // void on_message(const std::string &payload) {
-  //   if (payload == "ON") {
-  //     digitalWrite(2, HIGH);
-  //     publish("the/other/topic", "Hello World!");
-  //   } else {
-  //     digitalWrite(2, LOW);
-  //     publish("the/other/topic", "Goodby World!");
-  //   }
-  // }
 };
